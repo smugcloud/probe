c2342f27ef9bfc4a474b831d114574fe05ced269
Nick Leli <nleli@newrelic.com>
2018-08-27 16:56:21 -0700

    Moving the information retrieval to be done once.

--- main.go
+++ main.go
@@ -9,29 +9,39 @@ import (
 	"os/exec"
 )
 
-var port string
+var (
+	addr     string
+	port     string
+	hostname string
+	s        string
+	out      string
+)
 
 // Probe acks back details about the container
 func Probe(w http.ResponseWriter, req *http.Request) {
-	h, _ := os.Hostname()
-	s := "Hello from container: " + h + "\n"
 
-	cmd := "ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{ print $1}'"
-
-	out, err := exec.Command("sh", "-c", cmd).Output()
-	if err != nil {
-		log.Printf("Failed to execute command: %s", cmd)
-	}
-	s += "Local IP of container: " + string(out) + "\n"
 	io.WriteString(w, s)
 }
 
 func main() {
 	flag.Parse()
+	hostname, _ := os.Hostname()
+	s := "Hello from container: " + hostname + "\n"
+
+	cmd := "ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{ print $1}'"
+	out, err := exec.Command("sh", "-c", cmd).Output()
+	s += "Local IP of container: " + string(out) + "\n"
+
+	if err != nil {
+		log.Printf("Failed to execute command: %s", cmd)
+	}
+
 	http.HandleFunc("/", Probe)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func init() {
 	flag.StringVar(&port, "port", "9000", "Specify the port number the server will listen on.")
+	flag.StringVar(&addr, "addr", "127.0.0.1", "Specify the address the server will listen on.")
+
 }
